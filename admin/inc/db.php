cc7dbb4ee3605e7f34e8239c923f0e0641d04ac6
Mark Wisdom Reeves <46942868+markwisdomreeves@users.noreply.github.com>
2024-05-26 19:08:57 +0200

    Update db.php

--- admin/inc/db.php
+++ admin/inc/db.php
@@ -1,8 +1,4 @@
 <?php
-// $DSN = 'mysql:host = localhost; dbname=cms_blog';
-// $conn = new PDO( $DSN, 'root', '' );
-
-$DSN = 'mysql:host = localhost; dbname=u989259138_powliberia123';
-$conn = new PDO( $DSN, 'u989259138_powliberia123', 'AK#OJ+t27vM$' );
-
+$DSN = 'mysql:host = localhost; dbname=cms_blog';
+$conn = new PDO( $DSN, 'root', '' );
 ?>
